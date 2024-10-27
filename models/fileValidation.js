f6420250b18b7387ac9d321a4f5782077e3b5866
Guillaume Paran <guparan@gmail.com>
2024-10-27 17:54:20 +0100

    Update fileValidation.js

--- models/fileValidation.js
+++ models/fileValidation.js
@@ -1,5 +1,5 @@
 import { Meteor } from 'meteor/meteor';
-import { exec } from 'node:child_process';
+import { exec } from 'child_process';
 import { promisify } from 'node:util';
 import fs from 'fs';
 import FileType from 'file-type';
